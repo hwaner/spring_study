8bf9a4d8c5b41e460b55987279bff7bef00537de
hwaner <ljhwan8111@naver.com>
2022-04-27 00:10:21 +0900

    big picture end

--- hellospring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,11 +3,16 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author hwaner
  * @created at 2022/04/21 14:03
  * @description:
+ * 1. model test
+ * 2. mvc test
+ * 3. api test
  */
 @Controller
 public class HelloController {
@@ -16,5 +21,39 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc") // localhost:8080/hello-mvc?name=spring!
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // ResponseBody: http body 부에 직접 넣어주겠다.
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // -> "hello spring", template 엔진 처리 없이 그대로 내려감
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
